738004d4412041c08eba7fc90f36b09ab6bcbbdd
usershokhrukh <asxurovsit@gmail.com>
2026-01-09 14:08:06 +0500

    first api code python

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+@app.route('/test', methods=['POST']) # Manzil yaratish
+def my_function():
+    data = request.json # Konvertni ochib, ichidagi ma'lumotni olish
+    print(data)         # Terminalda ko'rish
+    return jsonify({"status": "olindi"})
